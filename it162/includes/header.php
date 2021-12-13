d83e2ec84dc14e61c454a2ba04244aa2c23d153c
kelseypatterson817 <87186451+kelseypatterson817@users.noreply.github.com>
2021-12-13 12:22:24 -0600

    Update header.php

--- it162/includes/header.php
+++ it162/includes/header.php
@@ -1,7 +1,8 @@
+<?php include 'portal-config.php'?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Kelsey Patterson's IT 162 Portal Website</title>
+<title><?=$title?></title>
 <meta name="viewport" content="width=device-width" />
 <meta name="robots" content="noindex,nofollow" />
 <meta charset="utf-8">
@@ -15,15 +16,18 @@
 <!-- START WRAPPER -->
 <main class="wrapper">
 <header>
-  <h1><a href="index.php"><i class="logo fa fa-home"></i> (Kelsey Patterson SCC IT162 Portal</a></h1>
+  <h1><a href="index.php"><i class="logo fa fa-home"></i> Kelsey's SCC IT162 Portal</a></h1>
   <nav>
     <ul class="topnav" id="myTopnav">
+    <?=makeLinks($nav1)?>
+	<!-- START COMMENT OUT NAV
       <li><a href="index.php" class="selected">Welcome</a></li>
       <li><a href="big/index.php">Big</a></li>
       <li><a href="aia.php">AIA</a></li>
       <li><a href="flowchart.php">Flowchart</a></li>
       <li><a href="fp/index.php">Final Project</a></li>
-      <li><a href="contactme.php">Contact Kelsey</a></li>
+      <li><a href="contactme.php">Contact Sara</a></li>
+	END COMMENT OUT NAV -->
       <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
     </ul>
   </nav>
